c9c4bde76f60bb3d0ced9e1d7ac1958124c5e0b7
Lakshya272-afk <157282808+Lakshya272-afk@users.noreply.github.com>
2025-07-30 14:07:14 +0530

    Update reverse.c
    adding code

--- reverse.c
+++ reverse.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number, reversed;
+
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &number);
+
+    reversed = (number % 10) * 100 + ((number / 10) % 10) * 10 + (number / 100);
+    
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
+}
 
